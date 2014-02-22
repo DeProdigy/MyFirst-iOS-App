26cdfe3e1410dc907c1ea8bed64abc83560032e8
DeProdigy <alex.hint.ah@gmail.com>
2014-02-22 16:03:46 -0500

    Initial Commit

---
+++ MyFirstApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by Alex Hint on 2/22/14.
+//  Copyright (c) 2014 Alex Hint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
